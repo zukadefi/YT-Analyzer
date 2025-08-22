93f06cafe29013beba3b164d198eb0985e3152b9
zuka <101676480+zukadefi@users.noreply.github.com>
2025-08-22 21:21:10 +0200

    Update v2.py

--- v2.py
+++ v2.py
@@ -89,7 +89,7 @@ if all(x > 0 for x in [implied_apy, underlying_apy, pt_price, d, yt_now]):
         y="Preço Atual YT"
     )
     
-    st.altair_chart(line + point, use_container_width=False)
+    st.altair_chart(line + point, use_container_width=True)
 
     qt = st.number_input("How many YT tokens are you buying?", min_value=0.0, step=1.0, format="%.2f")
     if qt > 0:
